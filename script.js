0e371a03c945cc12729f251e8516a8cb7f0557a1
Youssef N <ynasr0378@gmail.com>
2025-11-25 09:23:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -160,7 +160,6 @@ document.addEventListener('DOMContentLoaded', () => {
             easing: 'ease-in-out',
             once: true,
         });
-    }
+}
     // ------------------------------------------
-
 });
